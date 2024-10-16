76559eb71654d85cd6fd400420c2eec8979f6fba
Bianca Popa User <bianca.user@Biancas-MacBook-Pro.local>
2024-10-16 20:58:43 +0300

    Added the GuessingGame project and made first view

---
+++ GuessingGame/components/PrimaryButton.js
@@ -0,0 +1,40 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+function PrimaryButton({ children }) {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        android_ripple={{ color: "#ffa700" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#ff6700",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.6,
+  },
+});
+
+export default PrimaryButton;
