e236c4460f1e175e40da130916e408936e1432b8
Bianca Popa User <bianca.user@Biancas-MacBook-Pro.local>
2024-10-20 23:51:46 +0300

    Added authentication using Firebase and stored token on device

--- AuthenticationApp/screens/SignupScreen.js
+++ AuthenticationApp/screens/SignupScreen.js
@@ -1,7 +1,31 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useContext, useState } from "react";
+import { Alert } from "react-native";
+
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const authCtx = useContext(AuthContext);
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert("Authentication failed!", "Could not create user. Please check your input or try again later!")
+      setIsAuthenticating(false);
+    }
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
